8377c45ae2ca36afcdeff444f064b5cb69f618e6
Robin-Andrews <prandrews@hotmail.co.uk>
2017-01-06 15:43:53 +0000

    improved formatting for robbybird.php

--- robbybird.php
+++ robbybird.php
@@ -10,69 +10,55 @@
 
 function ra_add_post()
 {
-    $prev_post = get_page_by_title('Robby Bird', 'OBJECT', 'post');
-    
-    
-    if ($prev_post->post_status == false || $prev_post->post_status == 'trash') {    
-        $html = '<div id="game">
-				
-					<p>This post is generated entirely from a plugin. Visit <a href="https://github.com/Robin-Andrews/Robby-Bird-WordPress-Plugin">Github</a> for the source code.</p>
-				
-					<p id="instructions">Press \'f\' or click/tap on the game to make the bird fly. Avoid poles and bounderies. Enjoy!</p>
-					
-					<div id="container">
-
-					<div id="bird"></div>
-
-					<div id="pole_1" class="pole"></div>
-					<div id="pole_2" class="pole"></div>
-
-					</div>
-
-					<div id="score_div">
-						<p><b>Score: </b><span id="score">0</span></p>
-						<p><b>Speed: </b><span id="speed">10</span></p>
-					</div>
-
-					<button id="restart_btn">Restart</button>
-					
-        </div><!-- game -->';
-        
-        $new_post = array(
-            'post_title' => 'Robby Bird',
-            'post_content' => $html,
-            'post_status' => 'publish'
-						// in future maybe add SEO meta
-        );
-        
-        wp_insert_post($new_post);
-        
-        
-    }
+	$prev_post = get_page_by_title('Robby Bird', 'OBJECT', 'post');
+		
+	if ($prev_post->post_status == false || $prev_post->post_status == 'trash'):?>    
+		<div id="game">
+			<p>This post is generated entirely from a plugin. Visit <a href="https://github.com/Robin-Andrews/Robby-Bird-WordPress-Plugin">Github</a> for the source code.</p>
+			<p id="instructions">Press \'f\' or click/tap on the game to make the bird fly. Avoid poles and bounderies. Enjoy!</p>
+			<div id="container">
+				<div id="bird"></div>
+				<div class="pole" id="pole_1"></div>
+				<div class="pole" id="pole_2"></div>
+			</div>
+			<div id="score_div">
+				<p><b>Score:</b> <span id="score">0</span></p>
+				<p><b>Speed:</b> <span id="speed">10</span></p>
+			</div><button id="restart_btn">Restart</button>
+		</div><!-- game -->				
+	<?php endif;
+		
+	$new_post = array(
+		'post_title' => 'Robby Bird',
+		'post_content' => $html,
+		'post_status' => 'publish'
+		// in future maybe add SEO meta
+	);
+		
+	wp_insert_post($new_post);
+	}
 }
 
 register_activation_hook(__FILE__, ra_add_post);
 
-
 function ra_robbybird(){    
-    if (is_single('Robby Bird')) {
-			
-		 // Register the script like this for a plugin:
-			wp_register_script('flatty-bird-script', plugins_url('script.js', __FILE__), array(
-					'jquery'
-			));
-			
-			// register jquery mobile
-			wp_register_script('jquery-mobile-js', 'http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.js');
-			
-			// Register the style like this for a plugin:
-			wp_register_style('flatty-bird-style', plugins_url('style.css', __FILE__), array(), '', 'all');
-			
-			// load them all	
-			wp_enqueue_style('flatty-bird-style');
-			wp_enqueue_script('flatty-bird-script');
-			// wp_enqueue_script('jquery-mobile-js'); // seems to break site 
-    }
+	if (is_single('Robby Bird')) {
+		
+	 // Register the script like this for a plugin:
+		wp_register_script('flatty-bird-script', plugins_url('script.js', __FILE__), array(
+				'jquery'
+		));		
+		// register jquery mobile
+		wp_register_script('jquery-mobile-js', 'http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.js');
+		
+		// Register the style like this for a plugin:
+		wp_register_style('flatty-bird-style', plugins_url('style.css', __FILE__), array(), '', 'all');
+		
+		// load them all	
+		wp_enqueue_style('flatty-bird-style');
+		wp_enqueue_script('flatty-bird-script');
+		// wp_enqueue_script('jquery-mobile-js'); // seems to break site 
+	}
 }
 
 add_action('wp', 'ra_robbybird');
